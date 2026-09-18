7c32a2bf77a4cb67a942562f1afe4a24883e2ae9
Dusk-92 <romain.thomas92@proton.me>
2026-09-18 11:25:46 +0200

    Raise zone count to 140

--- Dusk/WhereToPlay/PlayerStats.lua
+++ Dusk/WhereToPlay/PlayerStats.lua
@@ -18,7 +18,7 @@ PlayerClass = Player:GetClass();
 settings["playerLvl"]["value"] = PlayerLvl;
 
 valCheck = 200;
-NbrEntries = 131;
+NbrEntries = 140;
 
 ------------------------------------------------------------------------------------------
 -- load all the dats needed
