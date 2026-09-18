1afcb858af191c67c4fbf98bcf97dd0baad88934
Dusk-92 <romain.thomas92@proton.me>
2026-09-18 21:51:47 +0200

    Use LevelChanged event instead of polling

--- Dusk/WhereToPlay/PlayerStats.lua
+++ Dusk/WhereToPlay/PlayerStats.lua
@@ -17,6 +17,48 @@ PlayerClass = Player:GetClass();
 
 settings["playerLvl"]["value"] = PlayerLvl;
 
-valCheck = 200;
 NbrEntries = 161;
+
+local WTP_LevelChangedHandler = nil;
+
+local function AddPlayerCallback(object, eventName, callback)
+	if(object == nil or callback == nil)then return; end
+	local current = object[eventName];
+	if(current == nil)then
+		object[eventName] = callback;
+	elseif(type(current) == "table")then
+		table.insert(current, callback);
+	else
+		object[eventName] = { current, callback };
+	end
+end
+
+local function RemovePlayerCallback(object, eventName, callback)
+	if(object == nil or callback == nil)then return; end
+	local current = object[eventName];
+	if(current == callback)then
+		object[eventName] = nil;
+	elseif(type(current) == "table")then
+		for i = table.getn(current), 1, -1 do
+			if(current[i] == callback)then
+				table.remove(current, i);
+				break;
+			end
+		end
+	end
+end
+
+function WhereToPlayAttachPlayerEvents()
+	if(WTP_LevelChangedHandler ~= nil or Player == nil)then return; end
+	WTP_LevelChangedHandler = function(sender, args)
+		UpdateLvl();
+	end
+	AddPlayerCallback(Player, "LevelChanged", WTP_LevelChangedHandler);
+end
+
+function WhereToPlayDetachPlayerEvents()
+	if(WTP_LevelChangedHandler == nil)then return; end
+	RemovePlayerCallback(Player, "LevelChanged", WTP_LevelChangedHandler);
+	WTP_LevelChangedHandler = nil;
+end
 
