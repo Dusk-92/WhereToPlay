c6972db03fac5eda08564046f627add78dbfb392
Dusk-92 <romain.thomas92@proton.me>
2026-09-18 14:17:09 +0200

    Harden settings migration and saving

--- Dusk/WhereToPlay/LoadAndSave.lua
+++ Dusk/WhereToPlay/LoadAndSave.lua
@@ -7,88 +7,71 @@
 -- create or load the settings
 ------------------------------------------------------------------------------------------
 function LoadSettings()
-	local _settings = PatchDataLoad(Turbine.DataScope.Character, "WhereToPlay_Settings", settings);
-    if (_settings ~= nil) then 
-		settings = _settings; 
+	local loadedSettings = PatchDataLoad(Turbine.DataScope.Character, settingsFileName or "WhereToPlay_Settings", settings);
+	if(type(loadedSettings) == "table")then
+		settings = loadedSettings;
 	end
-------------------------------------------------------------------------------------------
---- adding new vars in the settings file ---
-------------------------------------------------------------------------------------------
-    if ( not settings.isFarmWindowVisible or
-    not settings.displayInstances or
-    not settings.isInstanceWindowVisible or
-    not settings.playerLvl or
-    not settings.altEnable) then
-		settings = {
-            windowPosition = { 
-                xPos = settings["windowPosition"]["xPos"], 
-                yPos = settings["windowPosition"]["yPos"] 
-            },
-            IconPosition = { 
-                xPosIcon = settings["IconPosition"]["xPosIcon"] , 
-                yPosIcon = settings["IconPosition"]["yPosIcon"]  
-            },
-            isMinimizeEnabled = { 
-                isMinimizeEnabled = settings["isMinimizeEnabled"]["isMinimizeEnabled"] 
-            },
-            isWindowVisible = { 
-                isWindowVisible = true 
-            },
-            isOptionsWindowVisible = { 
-                isOptionsWindowVisible = false 
-            },
-            isMapWindowVisible = { 
-                value = false 
-            },
-            isFarmWindowVisible = { 
-                value = false 
-            },
-            isInstanceWindowVisible = { 
-                value = false 
-            },
-            displayInstances = { 
-                value = false 
-            },
-            verbose = { 
-                value = settings["verbose"]["value"] 
-            },
-            escEnable = { 
-                escEnable = settings["escEnable"]["escEnable"] 
-            },
-            altEnable = { 
-                altEnable = true 
-            },
-            playerLvl = { 
-                value = 0 
-            }
-        };
-	end
-	-- Transient windows are recreated on demand and must never persist as open across reloads.
-	settings["isOptionsWindowVisible"]["isOptionsWindowVisible"] = false;
-	settings["isMapWindowVisible"]["value"] = false;
-	settings["isFarmWindowVisible"]["value"] = false;
-	settings["isInstanceWindowVisible"]["value"] = false;
+
+	if(type(settings.windowPosition) ~= "table")then settings.windowPosition = {}; end
+	if(settings.windowPosition.xPos == nil)then settings.windowPosition.xPos = 500; end
+	if(settings.windowPosition.yPos == nil)then settings.windowPosition.yPos = 500; end
+
+	if(type(settings.IconPosition) ~= "table")then settings.IconPosition = {}; end
+	if(settings.IconPosition.xPosIcon == nil)then settings.IconPosition.xPosIcon = 500; end
+	if(settings.IconPosition.yPosIcon == nil)then settings.IconPosition.yPosIcon = 500; end
+
+	if(type(settings.isMinimizeEnabled) ~= "table")then settings.isMinimizeEnabled = {}; end
+	if(settings.isMinimizeEnabled.isMinimizeEnabled == nil)then settings.isMinimizeEnabled.isMinimizeEnabled = false; end
+
+	if(type(settings.isWindowVisible) ~= "table")then settings.isWindowVisible = {}; end
+	if(settings.isWindowVisible.isWindowVisible == nil)then settings.isWindowVisible.isWindowVisible = true; end
+
+	if(type(settings.isOptionsWindowVisible) ~= "table")then settings.isOptionsWindowVisible = {}; end
+	if(settings.isOptionsWindowVisible.isOptionsWindowVisible == nil)then settings.isOptionsWindowVisible.isOptionsWindowVisible = false; end
+
+	if(type(settings.isMapWindowVisible) ~= "table")then settings.isMapWindowVisible = {}; end
+	if(settings.isMapWindowVisible.value == nil)then settings.isMapWindowVisible.value = false; end
+
+	if(type(settings.isFarmWindowVisible) ~= "table")then settings.isFarmWindowVisible = {}; end
+	if(settings.isFarmWindowVisible.value == nil)then settings.isFarmWindowVisible.value = false; end
+
+	if(type(settings.isInstanceWindowVisible) ~= "table")then settings.isInstanceWindowVisible = {}; end
+	if(settings.isInstanceWindowVisible.value == nil)then settings.isInstanceWindowVisible.value = false; end
+
+	if(type(settings.displayInstances) ~= "table")then settings.displayInstances = {}; end
+	settings.displayInstances.value = false;
+
+	if(type(settings.verbose) ~= "table")then settings.verbose = {}; end
+	if(settings.verbose.value == nil)then settings.verbose.value = false; end
+
+	if(type(settings.escEnable) ~= "table")then settings.escEnable = {}; end
+	if(settings.escEnable.escEnable == nil)then settings.escEnable.escEnable = true; end
+
+	if(type(settings.altEnable) ~= "table")then settings.altEnable = {}; end
+	if(settings.altEnable.altEnable == nil)then settings.altEnable.altEnable = true; end
+
+	if(type(settings.playerLvl) ~= "table")then settings.playerLvl = {}; end
+	if(settings.playerLvl.value == nil)then settings.playerLvl.value = 0; end
+
+	-- Transient windows are recreated on demand and must never persist as open.
+	settings.isOptionsWindowVisible.isOptionsWindowVisible = false;
+	settings.isMapWindowVisible.value = false;
+	settings.isFarmWindowVisible.value = false;
+	settings.isInstanceWindowVisible.value = false;
 end
+
 ------------------------------------------------------------------------------------------
--- create the save settings
+-- save settings
 ------------------------------------------------------------------------------------------
 function SaveSettings()
-	settings["windowPosition"]["xPos"] = tostring(WhereToPlay:GetLeft());
-    settings["windowPosition"]["yPos"] = tostring(WhereToPlay:GetTop());
-    settings["IconPosition"]["xPosIcon"] = tostring(MainMinimizedIcon:GetLeft());
-   	settings["IconPosition"]["yPosIcon"] = tostring(MainMinimizedIcon:GetTop());
-	settings["isMinimizeEnabled"]["isMinimizeEnabled"] = settings["isMinimizeEnabled"]["isMinimizeEnabled"];
-	settings["isWindowVisible"]["isWindowVisible"] = settings["isWindowVisible"]["isWindowVisible"];
-    settings["isOptionsWindowVisible"]["isOptionsWindowVisible"] = settings["isOptionsWindowVisible"]["isOptionsWindowVisible"];
-	settings["isMapWindowVisible"]["value"] = settings["isMapWindowVisible"]["value"];
-	settings["isFarmWindowVisible"]["value"] = settings["isFarmWindowVisible"]["value"];
-    settings["isInstanceWindowVisible"]["value"] = settings["isInstanceWindowVisible"]["value"];
-    settings["isInstanceWindowVisible"]["value"] = settings["isInstanceWindowVisible"]["value"];
-    settings["verbose"]["value"] = settings["verbose"]["value"];
-    settings["escEnable"]["escEnable"] = settings["escEnable"]["escEnable"];
-    settings["altEnable"]["altEnable"] = settings["altEnable"]["altEnable"];
-    settings["displayInstances"]["value"] = settings["displayInstances"]["value"];
-    settings["playerLvl"]["value"] = settings["playerLvl"]["value"];
+	if(WhereToPlay ~= nil)then
+		settings.windowPosition.xPos = WhereToPlay:GetLeft();
+		settings.windowPosition.yPos = WhereToPlay:GetTop();
+	end
+	if(MainMinimizedIcon ~= nil)then
+		settings.IconPosition.xPosIcon = MainMinimizedIcon:GetLeft();
+		settings.IconPosition.yPosIcon = MainMinimizedIcon:GetTop();
+	end
 
-	PatchDataSave( Turbine.DataScope.Character, "WhereToPlay_Settings", settings);
+	PatchDataSave(Turbine.DataScope.Character, settingsFileName or "WhereToPlay_Settings", settings);
 end
